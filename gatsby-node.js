bde74f106f4750314045455d8d98d0ff0e227f05
lixi <1491812683@qq.com>
2021-01-24 16:48:35 +0800

    feat: mdx render custom components

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,11 @@
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const { createFilePath } = require(`gatsby-source-filesystem`);
+const docTemplate = require.resolve(`./src/templates/document.tsx`);
 
 // create URL slugs for markdown pages
 // https://www.gatsbyjs.com/plugins/gatsby-source-filesystem/
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
-  if (node.internal.type === `MarkdownRemark`) {
+  if (node.internal.type === `Mdx`) {
     const slug = createFilePath({ node, getNode, basePath: `charts/`, trailingSlash: false })
     createNodeField({
       node,
@@ -16,10 +17,9 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
-  const docTemplate = require.resolve(`./src/templates/document.tsx`)
   const result = await graphql(`
     {
-      allMarkdownRemark(
+      allMdx(
         sort: { order: ASC, fields: [frontmatter___order] }
         limit: 1000
       ) {
@@ -41,7 +41,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   }
 
   // create markdown page via template
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+  result.data.allMdx.edges.forEach(({ node }) => {
     createPage({
       path: node.fields.slug,
       component: docTemplate,
