317fde524a86407de37cffbb84179b13427ed62e
胡波 <751968840@qq.com>
2023-02-19 17:04:55 +0800

    docs: add props

--- src/constants/props.ts
+++ src/constants/props.ts
@@ -1,117 +1,226 @@
 export const chartKnowledges = [
     {
-        chartKnowledge: {
-            id: 'parallel_coordinates',
-            name: 'Parallel Coordinates',
-            alias: [],
-            family: ['LineCharts'],
-            def: '',
-            purpose: ['Distribution', 'Relation'],
-            coord: [],
-            category: ['Statistic'],
-            shape: ['Lines'],
-            dataPres: [{ minQty: 1, maxQty: 1, fieldConditions: ['Interval'] }],
-            channel: [],
-            recRate: "",
+        "chartKnowledge": {
+            "id": "parallel_coordinates",
+            "name": "Parallel Coordinates",
+            "alias": [],
+            "family": [
+                "LineCharts"
+            ],
+            "def": "",
+            "purpose": [
+                "Distribution",
+                "Relation"
+            ],
+            "coord": [],
+            "category": [
+                "Statistic"
+            ],
+            "shape": [
+                "Lines"
+            ],
+            "dataPres": [
+                {
+                    "minQty": 1,
+                    "maxQty": 1,
+                    "fieldConditions": [
+                        "Interval"
+                    ]
+                }
+            ],
+            "channel": [],
+            "recRate": ""
         },
-        trans: {
-            'zh-CN': {
-                name: '平行坐标系',
-                alias: [],
-                def: '平行坐标系，是一种含有多个垂直平行坐标轴的统计图表。',
-            }
-     
-        }
-    },
-    {
-        chartKnowledge: {
-            id: 'sunburst_chart',
-            name: 'Sunburst Chart',
-            alias: [],
-            family: ['LineCharts'],
-            def: '',
-            purpose: ['Composition'],
-            coord: [],
-            category: ['Statistic'],
-            shape: ['Round'],
-            dataPres: [{ minQty: 1, maxQty: 1, fieldConditions: ['Interval'] }],
-            channel: [],
-            recRate: "",
-        },
-        trans: {
-            'zh-CN': {
-                name: '旭日图',
-                alias: [],
-                def: '旭日图是一种表现层级数据的图表。',
+        "trans": {
+            "zh-CN": {
+                "name": "平行坐标系",
+                "alias": [],
+                "def": "平行坐标系，是一种含有多个垂直平行坐标轴的统计图表。"
             }
         }
     },
     {
-        chartKnowledge: {
-            id: 'hexagonal_binning',
-            name: 'Hexagonal Binning',
-            alias: [],
-            family: ['LineCharts'],
-            def: '',
-            purpose: ['Composition'],
-            coord: [],
-            category: ['Statistic'],
-            shape: ['Round'],
-            dataPres: [{ minQty: 1, maxQty: 1, fieldConditions: ['Interval'] }],
-            channel: [],
-            recRate: "",
+        "chartKnowledge": {
+            "id": "sunburst_chart",
+            "name": "Sunburst Chart",
+            "alias": [],
+            "family": [
+                "LineCharts"
+            ],
+            "def": "",
+            "purpose": [
+                "Composition"
+            ],
+            "coord": [],
+            "category": [
+                "Statistic"
+            ],
+            "shape": [
+                "Round"
+            ],
+            "dataPres": [
+                {
+                    "minQty": 1,
+                    "maxQty": 1,
+                    "fieldConditions": [
+                        "Interval"
+                    ]
+                }
+            ],
+            "channel": [],
+            "recRate": ""
         },
-        trans: {
-            'zh-CN': {
-                name: '六边形分箱图',
-                alias: [],
-                def: '六边形图，是一种由六边形为主要元素的统计图表',
+        "trans": {
+            "zh-CN": {
+                "name": "旭日图",
+                "alias": [],
+                "def": "旭日图是一种表现层级数据的图表。"
             }
         }
     },
     {
-        chartKnowledge: {
-            id: 'nightingale_rose_chart',
-            name: 'Nightingale Rose Chart',
-            alias: [],
-            family: ['LineCharts'],
-            def: '',
-            purpose: ['Composition'],
-            coord: [],
-            category: ['Statistic'],
-            shape: ['Round'],
-            dataPres: [{ minQty: 1, maxQty: 1, fieldConditions: ['Interval'] }],
-            channel: [],
-            recRate: "",
+        "chartKnowledge": {
+            "id": "hexagonal_binning",
+            "name": "Hexagonal Binning",
+            "alias": [],
+            "family": [
+                "LineCharts"
+            ],
+            "def": "",
+            "purpose": [
+                "Composition"
+            ],
+            "coord": [],
+            "category": [
+                "Statistic"
+            ],
+            "shape": [
+                "Round"
+            ],
+            "dataPres": [
+                {
+                    "minQty": 1,
+                    "maxQty": 1,
+                    "fieldConditions": [
+                        "Interval"
+                    ]
+                }
+            ],
+            "channel": [],
+            "recRate": ""
         },
-        trans: {
-            'zh-CN': {
-                name: '南丁格尔玫瑰图',
-                alias: [],
-                def: '南丁格尔玫瑰图又名鸡冠花图、极坐标区域图',
+        "trans": {
+            "zh-CN": {
+                "name": "六边形分箱图",
+                "alias": [],
+                "def": "六边形图，是一种由六边形为主要元素的统计图表"
             }
         }
     },
     {
-        chartKnowledge: {
-            id: 'stream_chart',
-            name: 'Stream Chart Rose',
-            alias: [],
-            family: [''],
-            def: '',
-            purpose: ['Trend'],
-            coord: [],
-            category: ['Statistic'],
-            shape: ['Lines'],
-            dataPres: [{ minQty: 1, maxQty: 1, fieldConditions: ['Interval'] }],
-            channel: [],
-            recRate: "",
+        "chartKnowledge": {
+            "id": "nightingale_rose_chart",
+            "name": "Nightingale Rose Chart",
+            "alias": [],
+            "family": [
+                "LineCharts"
+            ],
+            "def": "",
+            "purpose": [
+                "Composition"
+            ],
+            "coord": [],
+            "category": [
+                "Statistic"
+            ],
+            "shape": [
+                "Round"
+            ],
+            "dataPres": [
+                {
+                    "minQty": 1,
+                    "maxQty": 1,
+                    "fieldConditions": [
+                        "Interval"
+                    ]
+                }
+            ],
+            "channel": [],
+            "recRate": ""
         },
-        trans: {
-            'zh-CN': {
-                name: '河流图',
-                alias: [],
-                def: '河流图又名流图、主题河流图',
+        "trans": {
+            "zh-CN": {
+                "name": "南丁格尔玫瑰图",
+                "alias": [],
+                "def": "南丁格尔玫瑰图又名鸡冠花图、极坐标区域图"
+            }
+        }
+    },
+    {
+        "chartKnowledge": {
+            "id": "stream_chart",
+            "name": "Stream Chart Rose",
+            "alias": [],
+            "family": [
+                ""
+            ],
+            "def": "",
+            "purpose": [
+                "Trend"
+            ],
+            "coord": [],
+            "category": [
+                "Statistic"
+            ],
+            "shape": [
+                "Lines"
+            ],
+            "dataPres": [
+                {
+                    "minQty": 1,
+                    "maxQty": 1,
+                    "fieldConditions": [
+                        "Interval"
+                    ]
+                }
+            ],
+            "channel": [],
+            "recRate": ""
+        },
+        "trans": {
+            "zh-CN": {
+                "name": "河流图",
+                "alias": [],
+                "def": "河流图又名流图、主题河流图"
+            }
+        }
+    },
+    {
+        "chartKnowledge": {
+            "name": "test",
+            "alias": [],
+            "family": [
+                "LineCharts",
+                "ColumnCharts"
+            ],
+            "purpose": [
+                "Hierarchy"
+            ],
+            "category": [
+                "Statistic"
+            ],
+            "shape": [
+                "Lines"
+            ],
+            "id": "test",
+            "recRate": "Not Recommended",
+            "channel": []
+        },
+        "trans": {
+            "zh-CN": {
+                "name": "test",
+                "alias": [],
+                "def": ""
             }
         }
     }
